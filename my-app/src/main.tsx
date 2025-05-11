233e632fcb977063b19bbdb9a3707477efac8c18
Catnip <72100668+Catnip-01@users.noreply.github.com>
2025-05-11 17:05:50 +0000

    routing set up working

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from "./components/login.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <BrowserRouter> */}
+    <RouterProvider router={router} />
+    {/* </BrowserRouter> */}
+  </StrictMode>
+);
