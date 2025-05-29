1e098bfe03214234435b97556d4a7fc03ac81e42
tanu-04 <129984837+tanu-04@users.noreply.github.com>
2025-05-29 18:19:54 +0000

    Add CI workflow with tests and coverage2

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
+import { screen } from '@testing-library/react'
+
 import App from './App';
 
 test('renders hello world text', () => {
